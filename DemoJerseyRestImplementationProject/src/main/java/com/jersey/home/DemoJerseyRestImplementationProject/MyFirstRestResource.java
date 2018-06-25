eaccc06bcf072d2a44c5a9563a6e2f89efd485c9
abguha <abguha@ABGUHA-LAP.oradev.oraclecorp.com>
2018-06-25 16:59:28 +0530

    first commit

---
+++ DemoJerseyRestImplementationProject/src/main/java/com/jersey/home/DemoJerseyRestImplementationProject/MyFirstRestResource.java
@@ -0,0 +1,17 @@
+package com.jersey.home.DemoJerseyRestImplementationProject;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("hello")
+public class MyFirstRestResource {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String helloWorld() {
+		return "Hello World!";
+	}
+	
+}
